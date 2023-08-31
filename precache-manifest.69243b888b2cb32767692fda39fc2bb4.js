b40e637b566212153608f4d387cc4b5eb0de6456
prynsh7 <priyanshraj45@gmail.com>
2023-09-01 00:36:58 +0530

    Updates

---
+++ precache-manifest.69243b888b2cb32767692fda39fc2bb4.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c086d6c0e0197360af5d4b0403a1b997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e375d9907e68c98066af",
+    "url": "/static/css/2.af2598c0.chunk.css"
+  },
+  {
+    "revision": "86e240cb69709d49a7df",
+    "url": "/static/css/main.7f99a570.chunk.css"
+  },
+  {
+    "revision": "e375d9907e68c98066af",
+    "url": "/static/js/2.1b645200.chunk.js"
+  },
+  {
+    "revision": "86e240cb69709d49a7df",
+    "url": "/static/js/main.4bb09f22.chunk.js"
+  },
+  {
+    "revision": "7f2ab9312695db43eeca",
+    "url": "/static/js/runtime-main.76592ba1.js"
+  },
+  {
+    "revision": "1a6be6198e9a19c1b65d67f11b14339f",
+    "url": "/static/media/Lora-Bold.1a6be619.eot"
+  },
+  {
+    "revision": "5725d1b32a30bee4828de4aaff591120",
+    "url": "/static/media/Lora-Bold.5725d1b3.ttf"
+  },
+  {
+    "revision": "c9e4eb4eeef28f7afffdf33da3c952b3",
+    "url": "/static/media/Lora-Bold.c9e4eb4e.woff"
+  },
+  {
+    "revision": "d07eb50f7e517cc17e09ca4ce0f1a50f",
+    "url": "/static/media/Lora-Bold.d07eb50f.svg"
+  },
+  {
+    "revision": "d432108383a4f1e8a1adafdf0e9040b9",
+    "url": "/static/media/Lora-Bold.d4321083.woff2"
+  },
+  {
+    "revision": "119c62dc9aa40fb6ca73077cdfb16664",
+    "url": "/static/media/Lora-Regular.119c62dc.woff"
+  },
+  {
+    "revision": "45af71019afd132d8178526527046204",
+    "url": "/static/media/Lora-Regular.45af7101.eot"
+  },
+  {
+    "revision": "700e1da5f60c8084cbfe2240cb712bc7",
+    "url": "/static/media/Lora-Regular.700e1da5.ttf"
+  },
+  {
+    "revision": "9daa69d2481df6037d53259f4052d6ac",
+    "url": "/static/media/Lora-Regular.9daa69d2.svg"
+  },
+  {
+    "revision": "e973961b6e36b28d57f9c71062b1749d",
+    "url": "/static/media/Lora-Regular.e973961b.woff2"
+  },
+  {
+    "revision": "c9b5cd35a8a9d16442a870f535bd2077",
+    "url": "/static/media/UI.c9b5cd35.png"
+  },
+  {
+    "revision": "370f3c644e4d40552880772181dfdf04",
+    "url": "/static/media/Ubuntu-Bold.370f3c64.woff"
+  },
+  {
+    "revision": "496f3d38141618b96681f3c6f2803943",
+    "url": "/static/media/Ubuntu-Bold.496f3d38.woff2"
+  },
+  {
+    "revision": "7f5683a596723e3002dc87bc20df7c5f",
+    "url": "/static/media/Ubuntu-Bold.7f5683a5.eot"
+  },
+  {
+    "revision": "9a0c05c7489a572a9c094d293f9f9c2b",
+    "url": "/static/media/Ubuntu-Bold.9a0c05c7.ttf"
+  },
+  {
+    "revision": "ee9591c391d755f469c1f4cb8523829f",
+    "url": "/static/media/Ubuntu-Bold.ee9591c3.svg"
+  },
+  {
+    "revision": "40315a7ea60f7e405d1fc3a536546d93",
+    "url": "/static/media/Ubuntu-Regular.40315a7e.ttf"
+  },
+  {
+    "revision": "8877ed6b8cb1cb0d415589da34252d21",
+    "url": "/static/media/Ubuntu-Regular.8877ed6b.woff"
+  },
+  {
+    "revision": "90dd210f3321c3168fbb3c93c08cd7b8",
+    "url": "/static/media/Ubuntu-Regular.90dd210f.svg"
+  },
+  {
+    "revision": "a643c5f1b1fb20917ffe2cef3ba59f8f",
+    "url": "/static/media/Ubuntu-Regular.a643c5f1.woff2"
+  },
+  {
+    "revision": "b2ac3f825fb8bb26465b6ad370c235f5",
+    "url": "/static/media/Ubuntu-Regular.b2ac3f82.eot"
+  },
+  {
+    "revision": "1327e3ed58dcd077a82029e1f0a09800",
+    "url": "/static/media/a-b testing.1327e3ed.png"
+  },
+  {
+    "revision": "4950d1dc86bd401166caf796b23d26d8",
+    "url": "/static/media/analysis.4950d1dc.png"
+  },
+  {
+    "revision": "35490df1533680a9392931c25e0e9ad4",
+    "url": "/static/media/avatar.35490df1.jpg"
+  },
+  {
+    "revision": "dbbcca82b77487fe8fc0efb42ef2d12c",
+    "url": "/static/media/avatarRev.dbbcca82.jpg"
+  },
+  {
+    "revision": "58d818138451a3352ad6387ccc7a2ec9",
+    "url": "/static/media/bg-half-ring-center-modal.58d81813.svg"
+  },
+  {
+    "revision": "1fddb12bf00dfee9618b9cb28a562fbb",
+    "url": "/static/media/bg-half-ring-center.1fddb12b.svg"
+  },
+  {
+    "revision": "41ab4226f4fd6af89587431a50a53a4a",
+    "url": "/static/media/bg-half-ring-left.41ab4226.svg"
+  },
+  {
+    "revision": "8554339825c45bdac41045b9a8440bb3",
+    "url": "/static/media/bg-half-ring-right.85543398.svg"
+  },
+  {
+    "revision": "8572f97c249a411ee5670d53a6559e1c",
+    "url": "/static/media/bg-half-ring-up.8572f97c.svg"
+  },
+  {
+    "revision": "bc68ae45f0d16e9434bf3163986470e9",
+    "url": "/static/media/bg-line.bc68ae45.svg"
+  },
+  {
+    "revision": "e09705cef7ee58704d09803cb84ef12f",
+    "url": "/static/media/high-fidelity-wireframes.e09705ce.png"
+  },
+  {
+    "revision": "cd4ffaf10f6542f115e55d6b930abd4f",
+    "url": "/static/media/icon-arrow-down.cd4ffaf1.svg"
+  },
+  {
+    "revision": "3ec596a25f1d55be9b5a4163957ffc8b",
+    "url": "/static/media/mockup.3ec596a2.png"
+  },
+  {
+    "revision": "736439eefe0103585f75c95534dbb47f",
+    "url": "/static/media/mockup.736439ee.png"
+  },
+  {
+    "revision": "bbd083aca254cb17a53902d46d9687f4",
+    "url": "/static/media/mockup.bbd083ac.jpg"
+  },
+  {
+    "revision": "cc334c753974b6b98d2571be8de0e1a4",
+    "url": "/static/media/mockup.cc334c75.png"
+  },
+  {
+    "revision": "d65d356d9e147d337ecdb90882b0a5a9",
+    "url": "/static/media/oxer.d65d356d.svg"
+  },
+  {
+    "revision": "fba0f7c0e92b69fa8f1ffa1d5cc89db5",
+    "url": "/static/media/oxer.fba0f7c0.eot"
+  },
+  {
+    "revision": "e216bd53b7a1f76f7d829cc87975d900",
+    "url": "/static/media/quillbot-design_process.e216bd53.png"
+  },
+  {
+    "revision": "bda09593986ff594b9e85bac16fe58fc",
+    "url": "/static/media/quillbot-flow.bda09593.png"
+  },
+  {
+    "revision": "187280c7ebe0e95e408f5295bd5d14a5",
+    "url": "/static/media/userflow.187280c7.png"
+  }
+]);
